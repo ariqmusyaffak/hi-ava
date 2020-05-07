029a0235a4ccb5a1e1a26ee38ca5380de54e342a
ariqmusyaffak <krunchariq@gmail.com>
2020-05-07 13:27:43 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "ariqMK.v", quotes: "Whatever happens, be grateful dip!" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "Keep sharing positive vibes!." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I miss you." },
-    { author: "standinshd", quotes: "I'm sorry for missing you." },
+    { author: "ariqMK.v", quotes: "Whatever happens, be grateful Dip!" },
+    { author: "ariqMK", quotes: "Keep spirit Dip!" },
+    { author: "ariqMK", quotes: "Hey! I wuv chuuuu Dip!" },
+    { author: "ariqMK", quotes: "Keep sharing positive vibes, Dip!." },
+    { author: "ariqMK", quotes: "I may not with you everyday Dip, but I love you everyday WKWKWK." },
+    { author: "ariqMK", quotes: "I miss you." },
+    { author: "ariqMK", quotes: "I'm sorry for missing you." },
 ];
