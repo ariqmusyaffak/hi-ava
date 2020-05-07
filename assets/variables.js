4776ecc2296b2112afa3d47cfc21ba9bfae1989e
ariqmusyaffak <krunchariq@gmail.com>
2020-05-07 13:13:55 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Ndip", "Diva", "Beibbb", "Dipa", "My Queen"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
